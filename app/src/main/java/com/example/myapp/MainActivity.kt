23799f252361b5c6419a11ae0eaf0fbc57ca12de
ZhytomyrskaAnna <Zhytomyreti@gmail.com>
2025-09-09 15:34:46 +0300

    Add images to your Android app is done

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -13,11 +13,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapp.ui.theme.MyAppTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import com.example.myapp.R
+
 
 
 class MainActivity : ComponentActivity() {
@@ -58,7 +65,27 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box (modifier){
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale= ContentScale.Crop,
+            alpha =0.5F
+        )
+        GreetingText(
+            message=message,
+            from = from,
+            modifier= Modifier
+                .fillMaxSize()
+                .padding(8.dp)
         )
     }
 }
@@ -67,6 +94,8 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     MyAppTheme {
-        GreetingText(message = "Happy Birthday Sam!", from = "From Kate")
+        GreetingImage(message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text)
+        )
     }
 }
